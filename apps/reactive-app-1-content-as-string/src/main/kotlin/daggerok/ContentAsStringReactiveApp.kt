8e424c16bad1328e4619a7e57388c08c6ee9945f
Maksim Kostromin <daggerok@gmail.com>
2022-05-14 05:26:05 +0300

    Add reactive-app-1-content-as-string module.

---
+++ apps/reactive-app-1-content-as-string/src/main/kotlin/daggerok/ContentAsStringReactiveApp.kt
@@ -0,0 +1,18 @@
+package daggerok
+
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.util.Locale
+import java.util.TimeZone
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContentAsStringReactiveApp
+
+fun main(args: Array<String>) {
+    runApplication<ContentAsStringReactiveApp>(*args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.from(ZoneOffset.UTC)))
+        Locale.setDefault(Locale.US)
+    }
+}
---
+++ apps/reactive-app-1-content-as-string/src/test/kotlin/daggerok/ContentAsStringReactiveAppTests.kt
@@ -0,0 +1,32 @@
+package daggerok
+
+import daggerok.app.ReportItems
+import org.apache.logging.log4j.kotlin.logger
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import reactor.test.StepVerifier
+
+@SpringBootTest(
+    classes = [ContentAsStringReactiveApp::class],
+    webEnvironment = RANDOM_PORT,
+)
+class ContentAsStringReactiveAppTests @Autowired constructor(val reportItems: ReportItems) {
+
+    @BeforeEach
+    fun setUp() {
+        reportItems.deleteAll()
+            .subscribe { log.info { "Remove: $it" } }
+    }
+
+    @Test
+    fun `should test context`() {
+        // given
+        StepVerifier.create(reportItems.deleteAll())
+            .verifyComplete()
+    }
+
+    companion object { val log = logger() }
+}
