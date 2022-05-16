26cd0c50c4861206a498b5761d421ecc686f2338
Maksim Kostromin <daggerok@gmail.com>
2022-05-17 02:15:36 +0300

    Remove useless DTOs.

--- apps/reactive-app-2-upload-file/src/main/kotlin/daggerok/app/api.kt
+++ apps/reactive-app-2-upload-file/src/main/kotlin/daggerok/app/api.kt
@@ -3,14 +3,6 @@ package daggerok.app
 import com.fasterxml.jackson.annotation.JsonIgnore
 import java.time.Instant
 
-data class ReportItemDTO(
-    val id: Long? = null,
-    val jobId: Long = -1,
-    val name: String = "",
-    val content: String = "",
-    val lastModifiedAt: Instant? = null,
-)
-
 data class ReportItemDocument(
     val id: Long? = null,
     @JsonIgnore val jobId: Long = -1,
