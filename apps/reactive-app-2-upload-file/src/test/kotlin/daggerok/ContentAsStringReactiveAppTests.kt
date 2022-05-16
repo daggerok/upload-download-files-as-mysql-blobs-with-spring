5a102cad123aeef85624f5b0ce601ac9c6bced63
Maksim Kostromin <daggerok@gmail.com>
2022-05-16 20:00:31 +0300

    Add reactive-app-3-download-file module.

--- apps/reactive-app-2-upload-file/src/test/kotlin/daggerok/ContentAsStringReactiveAppTests.kt
+++ apps/reactive-app-2-upload-file/src/test/kotlin/daggerok/ContentAsStringReactiveAppTests.kt
@@ -10,10 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDO
 import reactor.test.StepVerifier
 
 @SpringBootTest(
-    classes = [ReactiveApp::class],
+    classes = [ContentAsStringReactiveApp::class],
     webEnvironment = RANDOM_PORT,
 )
-class ReactiveAppTests @Autowired constructor(val reportItems: ReportItems) {
+class ContentAsStringReactiveAppTests @Autowired constructor(val reportItems: ReportItems) {
 
     @BeforeEach
     fun setUp() {
---
+++ apps/reactive-app-3-download-file/src/main/kotlin/daggerok/app/api.kt
@@ -0,0 +1,31 @@
+package daggerok.app
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import java.time.Instant
+
+data class ReportItemDTO(
+    val id: Long? = null,
+    val jobId: Long = -1,
+    val name: String = "",
+    val content: String = "",
+    val lastModifiedAt: Instant? = null,
+)
+
+data class ReportItemDocument(
+    val id: Long? = null,
+    @JsonIgnore val jobId: Long = -1,
+    val name: String = "",
+    val content: String = "",
+    val lastModifiedAt: Instant? = null,
+)
+
+fun ReportItem.toDocument(): ReportItemDocument =
+    ReportItemDocument(id, jobId, name, content = String(bytes = content, Charsets.UTF_8), lastModifiedAt)
+
+data class UploadFileDocument(
+    @JsonIgnore val id: Long? = -1,
+    @JsonIgnore val filename: String = "",
+    val message: String = "File(id=$id, filename=$filename) was saved",
+)
+
+class MissingFileException : RuntimeException("File not found")
